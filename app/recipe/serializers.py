1dfb16f4d0891d2df1099e721867ebe63b1843f1
Dev_francis <okeke98@gmail.com>
2023-02-05 16:18:40 +0100

    upload image implemented

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -94,6 +94,16 @@ class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for Recipe detail view."""
 
     class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
+        fields = RecipeSerializer.Meta.fields + ['description', 'image']
+
+
+class RecipeImageSerializer(serializers.ModelSerializer):
+    """Serializer for uploading Recipe image."""
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'image']
+        read_only_fields = ['id']
+        extra_kwargs = {'image': {'required': 'True'}}
 
 
